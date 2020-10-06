da5385ba820d44121d09ea2e312f79fd81d69e2f
Shubhad Mathur <32930066+frontseat-astronaut@users.noreply.github.com>
2020-10-06 14:30:53 +0530

    added code for bounding boxes

--- generate/render_videos.py
+++ generate/render_videos.py
@@ -707,6 +707,14 @@ def add_random_objects(scene_struct, num_objects, args, camera):
 
         # Record data about the object in the scene data structure
         pixel_coords = utils.get_camera_coords(camera, obj.location)
+        
+        # Get 2D pixel coordinates for all 8 points in the bounding box
+        scene = bpy.context.scene
+        cam_ob = scene.camera
+        me_ob = bpy.context.object
+
+        bound_box = camera_view_bounds_2d(bpy.context.scene, cam_ob, me_ob)
+        
         objects.append({
             'shape': obj_name_out,
             'size': size_name,
@@ -717,6 +725,10 @@ def add_random_objects(scene_struct, num_objects, args, camera):
             'pixel_coords': pixel_coords,
             'color': color_name,
             'instance': obj.name,
+            'x': bound_box.x,
+            'y': bound_box.y,
+            'width': bound_box.width,
+            'height': bound_box.height
         })
     return objects, blender_objects
 
@@ -843,6 +855,116 @@ def add_flips(blender_objects, num_flips=10, total_frames=300):
             blender_objects, start_frame=current_frame, end_frame=end_frame)
         current_frame = end_frame + 1
     bpy.ops.screen.frame_jump(end=False)
+
+
+
+class Box:
+
+    dim_x = 1
+    dim_y = 1
+
+    def __init__(self, min_x, min_y, max_x, max_y, dim_x=dim_x, dim_y=dim_y):
+        self.min_x = min_x
+        self.min_y = min_y
+        self.max_x = max_x
+        self.max_y = max_y
+        self.dim_x = dim_x
+        self.dim_y = dim_y
+
+    @property
+    def x(self):
+        return round(self.min_x * self.dim_x)
+
+    @property
+    def y(self):
+        return round(self.dim_y - self.max_y * self.dim_y)
+
+    @property
+    def width(self):
+        return round((self.max_x - self.min_x) * self.dim_x)
+
+    @property
+    def height(self):
+        return round((self.max_y - self.min_y) * self.dim_y)
+
+    def __str__(self):
+        return "<Box, x=%i, y=%i, width=%i, height=%i>" % \
+               (self.x, self.y, self.width, self.height)
+
+    def to_tuple(self):
+        if self.width == 0 or self.height == 0:
+            return (0, 0, 0, 0)
+        return (self.x, self.y, self.width, self.height)
+
+
+def camera_view_bounds_2d(scene, cam_ob, me_ob):
+    """
+    Returns camera space bounding box of mesh object.
+    Negative 'z' value means the point is behind the camera.
+    Takes shift-x/y, lens angle and sensor size into account
+    as well as perspective/ortho projections.
+    :arg scene: Scene to use for frame size.
+    :type scene: :class:`bpy.types.Scene`
+    :arg obj: Camera object.
+    :type obj: :class:`bpy.types.Object`
+    :arg me: Untransformed Mesh.
+    :type me: :class:`bpy.types.Mesh´
+    :return: a Box object (call its to_tuple() method to get x, y, width and height)
+    :rtype: :class:`Box`
+    """
+
+    mat = cam_ob.matrix_world.normalized().inverted()
+    me = me_ob.to_mesh(scene, True, 'PREVIEW')
+    me.transform(me_ob.matrix_world)
+    me.transform(mat)
+
+    camera = cam_ob.data
+    frame = [-v for v in camera.view_frame(scene=scene)[:3]]
+    camera_persp = camera.type != 'ORTHO'
+
+    lx = []
+    ly = []
+
+    for v in me.vertices:
+        co_local = v.co
+        z = -co_local.z
+
+        if camera_persp:
+            if z == 0.0:
+                lx.append(0.5)
+                ly.append(0.5)
+            # Does it make any sense to drop these?
+            #if z <= 0.0:
+            #    continue
+            else:
+                frame = [(v / (v.z / z)) for v in frame]
+
+        min_x, max_x = frame[1].x, frame[2].x
+        min_y, max_y = frame[0].y, frame[1].y
+
+        x = (co_local.x - min_x) / (max_x - min_x)
+        y = (co_local.y - min_y) / (max_y - min_y)
+
+        lx.append(x)
+        ly.append(y)
+
+    min_x = clamp(min(lx), 0.0, 1.0)
+    max_x = clamp(max(lx), 0.0, 1.0)
+    min_y = clamp(min(ly), 0.0, 1.0)
+    max_y = clamp(max(ly), 0.0, 1.0)
+
+    bpy.data.meshes.remove(me)
+
+    r = scene.render
+    fac = r.resolution_percentage * 0.01
+    dim_x = r.resolution_x * fac
+    dim_y = r.resolution_y * fac
+
+    return Box(min_x, min_y, max_x, max_y, dim_x, dim_y)
+
+
+def clamp(x, minimum, maximum):
+    return max(minimum, min(x, maximum))
 
 
 def compute_all_relationships(scene_struct, eps=0.2):
